0488d9b4e8025de4c292306fdc06a42c4105fa6c
thien-0408 <nguyendinhthien933@gmail.com>
2025-06-05 11:17:07 +0700

    delete files

--- backend/AdapterPattern/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        AudioPlayer audioPlayer = new AudioPlayer();
-        audioPlayer.play("mp3", "song.mp3");
-        audioPlayer.play("vlc", "movie.vlc");
-        audioPlayer.play("mp4", "video.mp4");
-        audioPlayer.play("avi", "clip.avi");
-    }
-}
--- backend/AdapterPattern/src/MediaPlayer.java
+++
@@ -1,4 +0,0 @@
-// Target: Giao diện mà client sử dụng
-interface MediaPlayer {
-    void play(String audioType, String fileName);
-}
