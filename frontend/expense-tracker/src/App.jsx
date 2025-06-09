8531619c99fc306a1daca0eb085dfaa97b9d47bc
thien-0408 <nguyendinhthien933@gmail.com>
2025-06-09 16:32:53 +0700

    update again

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -1,8 +1,5 @@
 import React, { useState } from 'react';
 import HomePage from './components/HomePage';
-import AdminDashboard from './components/AdminDashboard';
-import { Home } from 'lucide-react';
-
 
 function App() {
     return (
